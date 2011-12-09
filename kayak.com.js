bbdaa336ffffbf7405cc64527ba465f1e16a167d
Steve Agalloco <steve.agalloco@gmail.com>
2011-12-09 13:19:02 -0500

    attempt to disable the annoying kayak popups

---
+++ kayak.com.js
@@ -0,0 +1,8 @@
+// console.log('ohai')
+$('#cbPRICLINE_FDCMP2').ajaxComplete(function(){
+  $(this).attr('checked', false);
+});
+
+$('#cbEXPEDIA_DFDCMP2').ajaxComplete(function(){
+  $(this).attr('checked', false);
+});
