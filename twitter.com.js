e3412b665b928400146066d42987414ff5311083
Steve Agalloco <steve.agalloco@gmail.com>
2011-12-09 13:19:19 -0500

    hide all the annoying fly.twitter.com crapola

---
+++ twitter.com.js
@@ -0,0 +1,5 @@
+setTimeout(function() {
+  $('div[data-component-term="user_recommendations"]').hide();
+  $('div[data-component-term="trends"]').hide();
+  $('div[data-component-term="footer"]').hide();
+}, 3000);
