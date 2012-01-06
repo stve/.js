50a2820338ff9e03e34a641834f9f360d62e8f53
Steve Agalloco <steve.agalloco@gmail.com>
2012-01-06 17:03:57 -0500

    hide the unread count

--- github.com.js
+++ github.com.js
@@ -1,12 +1,4 @@
 $(".sponsor").remove();
-
-$(".pagination a").css({
-  '-moz-border-radius': '3px',
-  '-webkit-border-radius': '3px',
-  'border-radius': '3px'
-});
-
-$('a.notifications_count').hide();
-
+$('span.unread_count').hide();
 $('div.bubble').css({'padding': '0px'});
 $('div#readme').css({'padding': '0px'});
