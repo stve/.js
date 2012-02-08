4a53f0198c5ee4692098d4e71acfd7baa85af5b8
Steve Agalloco <steve.agalloco@gmail.com>
2012-02-08 10:58:07 -0500

    hide the discover tab

--- twitter.com.js
+++ twitter.com.js
@@ -2,4 +2,5 @@ setTimeout(function() {
   $('div[data-component-term="user_recommendations"]').hide();
   $('div[data-component-term="trends"]').hide();
   $('div[data-component-term="footer"]').hide();
+  $('li[data-global-action="discover"]').hide();
 }, 3000);
