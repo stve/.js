335e6e7a56e59bd21077d921bbf8f29f1aa3804b
Steve Agalloco <steve.agalloco@gmail.com>
2012-09-11 21:41:44 -0400

    ditch ellipsis in feed

--- github.com.js
+++ github.com.js
@@ -1,3 +1,4 @@
 $(".sponsor").remove();
 $('span.unread_count').hide();
 $('div.bubble').css({'padding': '0px'});
+$('div#dashboard a').removeClass('css-truncate css-truncate-target'); //.css({'max-width' : 'none'});
